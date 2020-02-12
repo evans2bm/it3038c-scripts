67950e42b2222a050e86bfe058a8903c0f738df9
= <evans2bm@mail.uc.edu>
2020-02-11 20:30:37 -0500

    python lab

---
+++ Labs/Lab5.py
@@ -0,0 +1,8 @@
+print('Hello, what is your name?')
+myName = input()
+
+print(myName + ' is a good name. My name is Snake. How old are you?')
+myAge = input()
+myAgeSecs = (int(myAge) * 31556952)
+print("you are %s years old? %s is %s seconds!" % (myAge, myAge, myAgeSecs))
+
