dc818cdf5b46833cb4d2d51d6bac59a9e7fbd2dd
= <evans2bm@mail.uc.edu>
2020-03-05 13:32:07 -0500

    updated lab7

--- PowerShell/WifiToolsScript.psm1
+++ PowerShell/WifiToolsScript.psm1
@@ -1,17 +1,21 @@
 ﻿Install-Module -Name WifiTools
 
-Function IPConfig {
-    $IPConfig= Show-IPConfig
-    return $IPConfig
-    }
-
 Function PublicIP {
-    $PublicIP = Get-PublicIP
-    return $PublicIP
-    }
 
-Function InterfaceIP {
-    $InterfaceIP = Get-InterfaceIP
-    return $InterfaceIP
-    }
+$IP = Get-PublicIP
+Write-Host("The Public IP Address is: $IP")
+}
+    
+   
+Function InteraceIP {
 
+$IP= Get-InterfaceIP
+return $IP
+}
+
+Function WifiState {
+
+$Wifi= Get-WifiState
+return $Wifi
+
+}
